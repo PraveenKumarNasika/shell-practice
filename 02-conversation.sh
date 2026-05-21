ca57c31f5fa87336d015f13e4b212631a7ac78ce
praveenkumar <praveenkumar.tst@gmail.com>
2026-05-21 11:54:04 -0500

    Hello World

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Modi
+
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: I am doing good. How about you?"
+echo "$PERSON1: I am also doing good, Whats up?"
+echo "$PERSON2: Going for walk. will you be join?"
