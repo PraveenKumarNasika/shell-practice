d6c77ce093fa878f0b01bd13c309d0d136ebf17c
praveenkumar <praveenkumar.tst@gmail.com>
2026-05-21 11:57:32 -0500

    Hello World

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: I am doing good. How about you?"
+echo "$PERSON1: I am also doing good, Whats up?"
+echo "$PERSON2: Going for walk. will you be join?"
