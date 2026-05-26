9f8918fd488393b0368eb0bcbece97b2445cf2af
praveenkumar <praveenkumar.tst@gmail.com>
2026-05-26 11:17:55 -0500

    Hello World

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,3 +6,9 @@ NUM2=20
 Total=$(($NUM1+$NUM2))
 
 echo "Total is: $Total"
+
+Movies=("RRR" "PUSHPA" "DURANDHAR")
+echo "Movies are: ${Movies[@]}"
+echo "First Movie is ${Movies[0]}"
+echo "second Movie is ${Movies[1]}"
+echo "third Movie is ${Movies[2]}"
