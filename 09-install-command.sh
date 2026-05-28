9f03c3ab336991eac4cde39cf0d824054bb1d102
praveenkumar <praveenkumar.tst@gmail.com>
2026-05-27 19:22:05 -0500

    install command

--- 09-install-command.sh
+++ 09-install-command.sh
@@ -5,8 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Please run this with root access"
     exit 1
+fi
 
-dnf list avaailable nginx
+dnf list available nginx
     if [ $? -eq 0 ]; then
         echo "nginx is already installed...skipping"
     else
