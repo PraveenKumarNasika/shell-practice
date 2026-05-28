e13327a8e305338bf02aa09795c3f00a100e8aec
praveenkumar <praveenkumar.tst@gmail.com>
2026-05-27 19:30:52 -0500

    install command

--- 09-install-functions.sh
+++ 09-install-functions.sh
@@ -7,15 +7,20 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
+validate(){
+    if [ $2 -eq 0 ]; then 
+            echo "$2 is successfully installed"
+        else 
+            echo "$2 installation is failed"
+        fi
+}
+
+
 dnf list installed nginx
     if [ $? -eq 0 ]; then
         echo "nginx is already installed...skipping"
     else
         echo "nginx is going to install"
         dnf install nginx -y
-        if [ $? -eq 0 ]; then 
-            echo "nginx is successfully installed"
-        else 
-            echo "nginx installation is failed"
-        fi
+        validate nginx $?
     fi
