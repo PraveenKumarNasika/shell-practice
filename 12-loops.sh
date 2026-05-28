5a97e76f05aa2cec27bb1d59ad89d18aa9b49b51
praveenkumar <praveenkumar.tst@gmail.com>
2026-05-27 20:05:02 -0500

    loops

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,17 +3,19 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/Shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this with root access" | tee -a $LOGS_FILE
+    echo "$TIMESTAMP Please run this with root access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 validate(){
     if [ $2 -eq 0 ]; then 
-            echo "$1 is successfully installed" | tee -a $LOGS_FILE
+            echo "$TIMESTAMP $1 is successfully installed" | tee -a $LOGS_FILE
         else 
-            echo "$1 installation is failed" | tee -a $LOGS_FILE
+            echo "$TIMESTAMP $1 installation is failed" | tee -a $LOGS_FILE
+            exit 1
         fi
 }
 
@@ -21,9 +23,9 @@ for instance in $@
     do
         dnf list installed $instance
     if [ $? -eq 0 ]; then
-        echo "$instance is already installed...skipping" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP $instance is already installed...skipping" | tee -a $LOGS_FILE
     else
-        echo "$instance is going to install" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP $instance is going to install" | tee -a $LOGS_FILE
         dnf install $instance -y &>> $LOGS_FILE
         validate $instance $?
     fi
